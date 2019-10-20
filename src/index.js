8ad92c254e10a0dc8676fce93e0be6490f3e4000
TorabRamin <codelessramin@email.com>
2019-10-21 00:48:21 +0600

    form validation on signup page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const bp      = require('body-parser');
-
+const signup  = require('./controllers/signup');
 const app     = express();
 
+//Middlewares
+app.use(bp.json());
 
+//Routes
+app.use(signup);
 
 // Set enviroment variable
 const _port = process.env.PORT || 4000;
